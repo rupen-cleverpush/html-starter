b5f17559475f8b98c3d81998895292968e14486d
Rupen Cleverpush <r.jarsaniya@cleverpush.com>
2025-12-05 14:18:10 +0530

    change channel id

--- cleverpush-worker.js
+++ cleverpush-worker.js
@@ -1,1 +1,1 @@
-importScripts('https://static.cleverpush.com/channel/worker/mej3qrfkS5mNXysgr.js' + self.location.search);
+importScripts('https://static.cleverpush.com/channel/worker/ARp3s2QQdooiKs8Lp.js' + self.location.search);
